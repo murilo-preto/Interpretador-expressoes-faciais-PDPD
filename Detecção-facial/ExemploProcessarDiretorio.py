eef3d71071a900ece0e0d7c6418a51c4ea45676f
Murilo Preto <murilopreto2003@Outlook.com>
2021-08-29 11:10:47 -0300

    update - variaveis

--- Detecção-facial/ExemploProcessarDiretorio.py
+++ Detecção-facial/ExemploProcessarDiretorio.py
@@ -2,17 +2,20 @@
 import DetectorCascata
 import os
 
-#Inserir variáveis:
-caminho_diretorio = r"exemplo"
-caminho_cascata = r"exemplo"
-caminho_output = r"exemplo"
+#Variáveis
+path_diretorio = r"caminho_diretorio"
+path_cascata = "caminho_cascata"
+path_output = "caminho_saida"
 
 #Identifica todos os arquivos no diretorio:
-for diretorio, _, arquivos in os.walk(caminho_diretorio):
+for diretorio, _, arquivos in os.walk(path_diretorio):
     for arquivo in arquivos:
-        imagem = (os.path.join(diretorio, arquivo))
+        path_imagem = (os.path.join(diretorio, arquivo))
 
-        DetectorCascata.Recortar_faces(imagem, caminho_cascata, caminho_output)
+        DetectorCascata.Recortar_faces(
+            caminho_imagem=path_imagem,\
+            caminho_cascata=path_cascata,\
+            caminho_output=path_output)
 
 
 
