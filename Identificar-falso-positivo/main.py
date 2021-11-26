4b4ccf81acbb16f8708e8f1c998a512580886d6c
Murilo Preto <murilopreto2003@Outlook.com>
2021-11-26 07:44:51 -0300

    Create main.py

---
+++ Identificar-falso-positivo/main.py
@@ -0,0 +1,35 @@
+import pandas as pd
+import os
+import cv2 as cv
+import keyboard
+
+diretorio = "imagens"
+
+df = pd.DataFrame(columns = ["Imagem", "Situação"])
+
+for diretorio, _, arquivos in os.walk(diretorio):
+    for arquivo in arquivos:
+        imagem = (os.path.join(diretorio, arquivo))
+
+        img = cv.imread(imagem)        
+        imS = cv.resize(img, (960, 540))
+
+        cv.imshow(imagem, imS)
+        cv.waitKey(0)
+        cv.destroyAllWindows()
+
+        while True:
+            try:
+                if keyboard.is_pressed('c'):
+                    print(f'Imagem correta: {arquivo}')
+                    df.loc[df.shape[0]] = [arquivo, 'Mantida']
+                    break
+                elif keyboard.is_pressed('p'):
+                    print(f'Falso positivo {arquivo}')
+                    df.loc[df.shape[0]] = [arquivo, 'Mantida']
+                    os.remove(imagem)
+                    break
+            except:
+                break
+
+df.to_csv(path_or_buf=f"teste_dataframe.csv")
