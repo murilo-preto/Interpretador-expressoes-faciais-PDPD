4eb1fc57ff3f4fc84d1a76579cb4816df9970d9e
Murilo Preto <murilopreto2003@Outlook.com>
2022-01-06 11:21:11 -0300

    otimização e clareza

--- Deteccao-facial-e-alinhamento/main.py
+++ Deteccao-facial-e-alinhamento/main.py
@@ -4,30 +4,30 @@ import dlib
 import pandas as pd
 import time
 
-vizinhos_minimos = 6
+#Variaveis
+vizinhos_minimos = 4
 fator_escala = 1.1
 base_de_dados = "input-base-de-dados"
 output_recortada = "output-1-faces-recortadas"
 output_alinhada = "output-2-faces-alinhadas"
 modelo_opencv = "haarcascade_frontalface_default.xml"
 modelo_dlib = "shape_predictor_5_face_landmarks.dat"
-lst = []
+lst = [] #Lista para log de excecoes
 
-tempo_inicial = time.perf_counter()
+tempo_inicial = time.perf_counter() #Tempo inicial
 
 #OPENCV
-opencv_detec_facial = cv.CascadeClassifier(modelo_opencv) #Ler modelo de detec opencv
-
+opencv_detec_facial = cv.CascadeClassifier(modelo_opencv) #Importa o modelo de detecção opencv
 for diretorio, _, arquivos in os.walk(base_de_dados): #Identifica todos os arquivos no diretorio
     for arquivo in arquivos:
         caminho_imagem = (os.path.join(diretorio, arquivo))
-        print(caminho_imagem)
+        print("Detec facial OpenCV", caminho_imagem)
         
-        imagem = cv.imread(caminho_imagem) #Ler arquivo de imagem
-        imagem_cinza = cv.cvtColor(imagem, cv.COLOR_BGR2GRAY) #Converter imagem em tons de cinza
+        img = cv.imread(caminho_imagem)
+        img_cinza = cv.imread(caminho_imagem,0)
         
         #Detectar faces:
-        faces = opencv_detec_facial.detectMultiScale (imagem_cinza, scaleFactor=fator_escala, minNeighbors=vizinhos_minimos, minSize=(500,500))
+        faces = opencv_detec_facial.detectMultiScale (img_cinza, scaleFactor=fator_escala, minNeighbors=vizinhos_minimos, minSize=(500,500))
 
         #Configurar nome:
         contador = 0
@@ -37,20 +37,20 @@ for diretorio, _, arquivos in os.walk(base_de_dados): #Identifica todos os arqui
         
         for (x, y, w, h) in faces: #Recortar imagens detectadas
             contador = contador+1
-            imagem_recortada = imagem[y:y+h, x:x+w]
+            imagem_recortada = img[y:y+h, x:x+w]
 
             #Escrever imagem para diretório
             caminho_recortada_output = os.path.join(output_recortada, f"{name}_{contador}.{ext}")
             cv.imwrite(filename=caminho_recortada_output, img=imagem_recortada)
 
 #DLIB
-previsor_formato = dlib.shape_predictor(modelo_dlib)
+previsor_formato = dlib.shape_predictor(modelo_dlib) #Importa o previsor de formato dlib
 detector_facial = dlib.get_frontal_face_detector()
 
 for diretorio, _, arquivos in os.walk(output_recortada): #Identifica todos os arquivos no diretorio
     for arquivo in arquivos:
         caminho_imagem = (os.path.join(diretorio, arquivo))
-        print(caminho_imagem)
+        print("Alinhamento facial DLIB", caminho_imagem)
 
         #Configurar nome:
         name = caminho_imagem.split("\\")
@@ -66,21 +66,23 @@ for diretorio, _, arquivos in os.walk(output_recortada): #Identifica todos os ar
             for detection in dets:
                 faces.append(previsor_formato(img, detection))
 
-            caminho_imagem_alinhada_saida = os.path.join(output_alinhada, f"{name}.{ext}") #Exportar imagem alinhada
+            caminho_imagem_alinhada_saida = os.path.join(output_alinhada, arquivo) #Exportar imagem alinhada
             images = dlib.get_face_chips(img, faces, size=1000)
+
             for image in images:
-                image_rgb = cv.cvtColor(image, cv.COLOR_BGR2RGB)
-                cv.imwrite(caminho_imagem_alinhada_saida, image_rgb)
+                img_cinza = cv.cvtColor(image, cv.COLOR_BGR2GRAY) #Converter imagem em tons de cinza
+                eq = cv.equalizeHist(img_cinza)
+                cv.imwrite(caminho_imagem_alinhada_saida, eq)
 
         except:
             print(f"Exceção: {name}")
             lst.append(arquivo)
 
-df = pd.DataFrame(lst, columns=["Excecoes"]) #Anotar exceções
-df.to_csv(path_or_buf="excecoes.csv")
+tempo_final = time.perf_counter() #Tempo final
+variacao_tempo = tempo_final-tempo_inicial #Calcular tempo de execução
 
-tempo_final = time.perf_counter()
-variacao_tempo = tempo_final-tempo_inicial
-
-with open('time-log.txt', 'w') as f:
+with open('time-log.txt', 'w') as f: #Exportar tempo em arquivo de txt
     f.write(str(variacao_tempo))
+
+df = pd.DataFrame(lst, columns=["Excecoes"]) #Exportar exceções em tabela
+df.to_csv(path_or_buf="excecoes.csv")
